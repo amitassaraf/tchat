5d7911f8a252f867e0b857b269adf817315a00a8
Amit Assaraf <amit@helpi.me>
2016-10-09 20:18:01 +0300

    Changed to debug

--- src/tchat/app.py
+++ src/tchat/app.py
@@ -10,4 +10,4 @@ app.config.from_object('tchat.settings')
 app.register_blueprint(api_blueprint)
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(host='0.0.0.0', port=8080, debug=True)
